3b0d78dfd0ea568cc5aa73a09d04c7111ea561f2
Sunki Baek <sunki.baek@gmail.com>
2018-08-24 11:39:12 -0700

    Edit to parse native callback result
    * Return error as Error object
    * Return result as js object
    * Edit typing info accordingly

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,22 @@
-interface callbackType {
-  (err: string, result: string): void
+interface ICallback<T> {
+  (error: Error | undefined, result: T): void
+}
+
+interface ITokenInfo {
+  token: string;
+}
+
+interface IProfile {
+  id: string;
+  nickname: string;
+  email: string | null;
+  display_id: string | null;
+  phone_number: string | null;
+  email_verified: boolean;
+  kakaotalk_user: boolean;
+  profile_image_path: string | null;
+  thumb_image_path: string | null;
+  has_signed_up:  boolean;
 }
 
 /**
@@ -7,18 +24,18 @@ interface callbackType {
  * @param {callbackType} callback callback received from native module
  * @returns {void}
  */
-export function login(callback: callbackType): void;
+export function login(callback: ICallback<ITokenInfo>): void;
 
 /**
  * logout
  * @param {callbackType} callback callback received from native module
  * @returns {void}
  */
-export function logout(callback: callbackType): void;
+export function logout(callback: ICallback<string>): void;
 
 /**
  * getProfile
  * @param {callbackType} callback callback received from native module. id, nickname, email, display_id, phone_number, email_verified, kakatalk_user, profile_image_path, has_signed_up values will be received with json string in result field.
  * @returns {void}
  */
-export function getProfile(callback: callbackType): void;
+export function getProfile(callback: ICallback<IProfile>): void;
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-
 import { NativeModules } from 'react-native';
 
 const { RNKakaoLogins } = NativeModules;
 
+const processNativeOutput = (userCallback) => {
+  return (errorString, resultString) => {
+    let error;
+
+    if (errorString) {
+      error = new Error(errorString);
+    }
+
+    const resultJSON = JSON.parse(resultString);
+
+    userCallback(error, resultJSON);
+  };
+}
+
 const KakaoLogins = {
   login(callback) {
-    RNKakaoLogins.login(callback);
+    RNKakaoLogins.login(processNativeOutput(callback));
   },
   logout(callback) {
-    RNKakaoLogins.logout(callback);
+    RNKakaoLogins.logout(processNativeOutput(callback));
   },
   getProfile(callback) {
-    RNKakaoLogins.getProfile(callback);
+    RNKakaoLogins.getProfile(processNativeOutput(callback));
   },
 };
 
