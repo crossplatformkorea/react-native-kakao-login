dfe90580e73fcb536f3e27caf9d9905fb79b068f
bang9dev <gusrn1423@naver.com>
2019-08-30 06:09:30 +0900

    feat(App): Example update using hooks

--- KakaoLoginExample/App.js
+++ KakaoLoginExample/App.js
@@ -1,132 +1,166 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import {
-  Platform,
-  Alert,
-  StyleSheet,
-  Text,
-  View,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image
 } from 'react-native';
 
 import RNKakaoLogins from 'react-native-kakao-logins';
 import NativeButton from 'apsl-react-native-button';
 
-export default class App extends Component<{},{}> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isKakaoLogging: false,
-      token: 'token has not fetched',
+if (!RNKakaoLogins) {
+    console.error('Module is Not Linked');
+}
+
+const logCallback = (log,callback) => {
+    console.log(log);
+    callback;
+};
+
+const TOKEN_EMPTY = "token has not fetched";
+const PROFILE_EMPTY = {id:'profile has not fetched', email:'profile has not fetched', profile_image_path:null};
+
+export default App = () => {
+    const [loginLoading, setLoginLoading] = useState(false);
+    const [logoutLoading, setLogoutLoading] = useState(false);
+    const [profileLoading, setProfileLoading] = useState(false);
+
+    const [token, setToken] = useState(TOKEN_EMPTY);
+    const [profile, setProfile] = useState(PROFILE_EMPTY);
+
+    const kakaoLogin = () => {
+        logCallback('Login Start', setLoginLoading(true));
+
+        RNKakaoLogins.login((err, result) => {
+            if (err){
+                return logCallback(`Login Failed:${err.toString()}`, setLoginLoading(false));
+            }
+            setToken(result.token);
+            logCallback(`Login Finished:${result.token}`, setLoginLoading(false));
+        });
     };
-    if (!RNKakaoLogins) {
-      console.log('Not Linked');
-    }
-  }
 
-  // 카카오 로그인 시작.
-  kakaoLogin() {
-    console.log('   kakaoLogin   ');
-    RNKakaoLogins.login((err, result) => {
-      if (err){
-        Alert.alert('error', err.toString());
-        return;
-      }
-      Alert.alert('result', result);
-    });
-  }
+    const kakaoLogout = () => {
+        logCallback('Logout Start', setLogoutLoading(true));
 
-  kakaoLogout() {
-    console.log('   kakaoLogout   ');
-    RNKakaoLogins.logout((err, result) => {
-      if (err){
-        Alert.alert('error', err.toString());
-        return;
-      }
-      Alert.alert('result', result);
-    });
-  }
+        RNKakaoLogins.logout((err, result) => {
+            if (err){
+                return logCallback(`Logout Failed:${err.toString()}`, setLogoutLoading(false));
+            }
+            setToken(TOKEN_EMPTY);
+            setProfile(PROFILE_EMPTY);
+            logCallback(`Logout Finished:${result}`, setLogoutLoading(false));
+        });
+    };
 
-  // 로그인 후 내 프로필 가져오기.
-  getProfile() {
-    console.log('getKakaoProfile');
-    RNKakaoLogins.getProfile((err, result) => {
-      if (err){
-        Alert.alert('error', err.toString());
-        return;
-      }
-      Alert.alert('result', result);
-    });
-  }
+    const getProfile = () => {
+        logCallback('Get Profile Start', setProfileLoading(true));
 
-  render() {
+        RNKakaoLogins.getProfile((err, result) => {
+            if (err){
+                return logCallback(`Get Profile Failed:${err.toString()}`, setProfileLoading(false));
+            }
+            setProfile(result);
+            logCallback(`Get Profile Finished:${JSON.stringify(result)}`, setProfileLoading(false));
+        });
+    };
+
+    const {id, email, profile_image_path:photo} = profile;
+
     return (
-      <View style={ styles.container }>
-        <View style={ styles.header }>
-          <Text>LOGIN</Text>
+        <View style={ styles.container }>
+            <View style={styles.profile}>
+                <Image
+                    style={styles.profilePhoto}
+                    source={{uri:photo}}
+                />
+                <Text>{`id : ${id}`}</Text>
+                <Text>{`email : ${email}`}</Text>
+            </View>
+            <View style={ styles.content}>
+                <Text style={styles.token}>{token}</Text>
+                <NativeButton
+                    isLoading={loginLoading}
+                    onPress={kakaoLogin}
+                    activeOpacity={0.5}
+                    style={styles.btnKakaoLogin}
+                    textStyle={styles.txtKakaoLogin}
+                >
+                    LOGIN
+                </NativeButton>
+                <NativeButton
+                    isLoading={logoutLoading}
+                    onPress={kakaoLogout}
+                    activeOpacity={0.5}
+                    style={styles.btnKakaoLogin}
+                    textStyle={styles.txtKakaoLogin}
+                >
+                    Logout
+                </NativeButton>
+                <NativeButton
+                    isLoading={profileLoading}
+                    onPress={getProfile}
+                    activeOpacity={0.5}
+                    style={styles.btnKakaoLogin}
+                    textStyle={styles.txtKakaoLogin}
+                >
+                    getProfile
+                </NativeButton>
+            </View>
         </View>
-        <View style={ styles.content }>
-          <NativeButton
-            isLoading={this.state.isNaverLoggingin}
-            onPress={() => this.kakaoLogin()}
-            activeOpacity={0.5}
-            style={styles.btnKakaoLogin}
-            textStyle={styles.txtNaverLogin}
-          >LOGIN</NativeButton>
-          <Text>{this.state.token}</Text>
-          <NativeButton
-            onPress={() => this.kakaoLogout()}
-            activeOpacity={0.5}
-            style={styles.btnKakaoLogin}
-            textStyle={styles.txtNaverLogin}
-          >Logout</NativeButton>
-          <NativeButton
-            isLoading={this.state.isKakaoLogging}
-            onPress={() => this.getProfile()}
-            activeOpacity={0.5}
-            style={styles.btnKakaoLogin}
-            textStyle={styles.txtNaverLogin}
-          >getProfile</NativeButton>
-        </View>
-      </View>
     );
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    marginTop: Platform.OS === 'ios' ? 0 : 24,
-    paddingTop: Platform.OS === 'ios' ? 24 : 0,
-    backgroundColor: 'white',
-  },
-  header: {
-    flex: 8.8,
-    flexDirection: 'row',
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  content: {
-    flex: 87.5,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignSelf: 'stretch',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  btnKakaoLogin: {
-    height: 48,
-    width: 240,
-    alignSelf: 'center',
-    backgroundColor: '#F8E71C',
-    borderRadius: 0,
-    borderWidth: 0,
-  },
-  txtNaverLogin: {
-    fontSize: 16,
-    color: '#3d3d3d',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        marginTop: Platform.OS === 'ios' ? 0 : 24,
+        paddingTop: Platform.OS === 'ios' ? 24 : 0,
+        backgroundColor: 'white',
+    },
+    profile: {
+        flex: 4,
+        alignItems:'center',
+        justifyContent:'flex-end',
+    },
+    profilePhoto: {
+        width:120,
+        height:120,
+        borderWidth:1,
+        borderColor:'black'
+    },
+    content: {
+        flex: 6,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    token: {
+        width:200,
+        fontSize:12,
+        padding:5,
+        borderRadius:8,
+        marginVertical:20,
+        backgroundColor:'grey',
+        color:'white',
+        textAlign:'center'
+    },
+    btnKakaoLogin: {
+        height: 48,
+        width: 240,
+        alignSelf: 'center',
+        backgroundColor: '#F8E71C',
+        borderRadius: 0,
+        borderWidth: 0,
+    },
+    txtKakaoLogin: {
+        fontSize: 16,
+        color: '#3d3d3d',
+    },
 });
