ed46a5d2bf3956fa3d693eb90297416552509e86
Johan Kim <hiphapis@gmail.com>
2020-07-16 10:22:46 +0900

    Add birthday and gender to IProfile (#148)
    Releated https://github.com/react-native-seoul/react-native-kakao-login/pull/102/files

--- index.ts
+++ index.ts
@@ -24,6 +24,8 @@ export interface IProfile {
   is_email_verified: boolean | null;
   is_kakaotalk_user: boolean | null;
   has_signed_up: boolean | null;
+  birthday: string | null;
+  gender: string | null;
 }
 
 /**
