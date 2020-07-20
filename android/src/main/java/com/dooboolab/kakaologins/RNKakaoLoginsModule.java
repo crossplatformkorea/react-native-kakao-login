73246d64a9e1488bb899dde9b46b15561eae7a65
Dan Maas <danmaas@users.noreply.github.com>
2020-07-19 18:07:46 -0700

    Always add callbacks in onHostResume() (#156)
    onHostDestroy() removes callbacks, so onHostResume() should always add them again, even if the Kakao SDK is already initialized.
    
    Without this fix, if the app's main activity is destroyed and re-created (because the app went into the background temporarily), then loginPromise will never resolve or reject.

--- android/src/main/java/com/dooboolab/kakaologins/RNKakaoLoginsModule.java
+++ android/src/main/java/com/dooboolab/kakaologins/RNKakaoLoginsModule.java
@@ -483,9 +483,9 @@ public class RNKakaoLoginsModule extends ReactContextBaseJavaModule implements A
             KakaoSDK.init(new KakaoSDKAdapter(reactContext.getApplicationContext()));
             reactContext.addActivityEventListener(this);
             callback = new SessionCallback();
-            Session.getCurrentSession().addCallback(callback);
-            Session.getCurrentSession().checkAndImplicitOpen();
         }
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
     }
 
     public static String getLoginErrorCode(KakaoException exception) {
