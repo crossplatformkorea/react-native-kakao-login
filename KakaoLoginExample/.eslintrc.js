ee854bb6c9a42c894d28c09f403a98049fe4b335
bang9 <gusrn1423@naver.com>
2022-11-08 11:47:31 +0900

    chore: update web sample

--- KakaoLoginExample/.eslintrc.js
+++ KakaoLoginExample/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'react-native/no-single-element-style-arrays': 2,
     'prettier/prettier': 0,
     'eol-last': 0,
+    "no-alert": 0,
     quotes: 0,
     '@typescript-eslint/no-unused-vars': 'off',
     'no-trailing-spaces': 0,
