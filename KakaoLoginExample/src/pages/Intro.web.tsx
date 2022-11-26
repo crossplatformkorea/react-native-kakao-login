c3d4e6793c220b7068301c573b5a99965c248838
Hyungu Kang | Airen <gusrn1423@naver.com>
2022-11-26 13:28:24 +0900

    refactor: extract logic by platforms using interface (#345)
    파일별로 플랫폼별 로직을 분리합니다.
    
    플랫폼별 메소드 인터페이스가 달라서 생기는 문제를, 임시로 오버로드를 통해서 분리하여 해결합니다.
    
    사용하는 프로젝트에서 확장자별 파일을 찾을 수 있도록 package.json 의 main 필드를 업데이트 합니다.
    
    - May resolve #337

--- KakaoLoginExample/src/pages/Intro.web.tsx
+++ KakaoLoginExample/src/pages/Intro.web.tsx
@@ -1,45 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { View } from 'react-native';
-import { login, getProfile as getKakaoProfile, logout, unlink, } from '@react-native-seoul/kakao-login';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import {View} from 'react-native';
+import {
+  login,
+  getProfile as getKakaoProfile,
+  logout,
+  unlink,
+} from '@react-native-seoul/kakao-login';
 import ResultViewWeb from './ResultViewWeb';
-import {KakaoProfileWebType} from "@react-native-seoul/kakao-login/src";
 
-const RestApiKey = "aa0293e157b3b865a943deade9a3c1a8";
-const redirectUrl = "http://localhost:3000";
+const RestApiKey = 'aa0293e157b3b865a943deade9a3c1a8';
+const redirectUrl = 'http://localhost:3000';
 const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${RestApiKey}&redirect_uri=${redirectUrl}&response_type=code`;
 
 function Login() {
   const location = useLocation();
   const [token, setToken] = useState<string>('');
-  const [type, setType] = useState<"login" | "profile" | "logout" | "unlink">();
+  const [type, setType] = useState<'login' | 'profile' | 'logout' | 'unlink'>();
   const [state, setState] = useState<any>();
 
   const signInWithKakao = async (codeWeb: string): Promise<void> => {
     try {
-      //@ts-ignore
-      const { access_token } = await login({ restApiKeyWeb: RestApiKey, redirectUrlWeb: redirectUrl, codeWeb });
-      setType("login");
+      const {access_token} = await login({
+        restApiKeyWeb: RestApiKey,
+        redirectUrlWeb: redirectUrl,
+        codeWeb,
+      });
+      setType('login');
       setState(access_token);
       setToken(access_token);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error, 'error');
     }
   };
 
   const signOutWithKakao = async (tokenWeb: string): Promise<void> => {
     if (!tokenWeb) {
-      //@ts-ignore
-      // eslint-disable-next-line no-alert
-      alert("로그인을 먼저 진행해 주세요");
+      alert('로그인을 먼저 진행해 주세요');
       return;
     }
     try {
       const message = await logout(tokenWeb);
-      setType("logout");
+      setType('logout');
       setState(message);
-      setToken("");
+      setToken('');
     } catch (err) {
       console.error('signOut error', err);
     }
@@ -47,33 +51,28 @@ function Login() {
 
   const getProfile = async (tokenWeb: string): Promise<void> => {
     if (!tokenWeb) {
-      //@ts-ignore
-      // eslint-disable-next-line no-alert
-      alert("로그인을 먼저 진행해 주세요");
+      alert('로그인을 먼저 진행해 주세요');
       return;
     }
     try {
-      const data = await getKakaoProfile(tokenWeb) as KakaoProfileWebType;
-      setType("profile");
+      const data = await getKakaoProfile(tokenWeb);
+      setType('profile');
       setState(data.properties);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error, 'error');
     }
   };
 
   const unlinkKakao = async (tokenWeb: string): Promise<void> => {
     if (!tokenWeb) {
-      //@ts-ignore
-      // eslint-disable-next-line no-alert
-      alert("로그인을 먼저 진행해 주세요");
+      alert('로그인을 먼저 진행해 주세요');
       return;
     }
     try {
       const message = await unlink(tokenWeb);
-      setType("unlink");
+      setType('unlink');
       setState(message);
-      setToken("");
+      setToken('');
     } catch (err) {
       console.error('signOut error', err);
     }
@@ -81,28 +80,30 @@ function Login() {
 
   useEffect(() => {
     if (location.search) {
-      signInWithKakao(location.search.split("code=")[1]);
+      signInWithKakao(location.search.split('code=')[1]);
     }
   }, []);
 
   return (
-    <View style={{
-      width: 786,
-      height: "100vh",
-      justifyContent: "center", alignItems: "center",
-      alignSelf: "center",
-    }}>
+    <View
+      style={{
+        width: 786,
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+      }}>
       <ResultViewWeb type={type} state={state} />
       <a
         href={kakaoUrl}
         style={{
           minWidth: 250,
-          padding: "10px 0px",
+          padding: '10px 0px',
           borderRadius: 5,
-          backgroundColor: "#F1DC11",
+          backgroundColor: '#F1DC11',
           fontSize: 20,
-          textDecoration: "none",
-          textAlign: "center"
+          textDecoration: 'none',
+          textAlign: 'center',
         }}>
         카카오 로그인
       </a>
@@ -111,12 +112,12 @@ function Login() {
         style={{
           minWidth: 250,
           marginTop: 10,
-          padding: "10px 20px",
+          padding: '10px 20px',
           borderRadius: 5,
-          backgroundColor: "#F1DC11",
+          backgroundColor: '#F1DC11',
           fontSize: 20,
-          border: "none",
-          cursor: 'pointer'
+          border: 'none',
+          cursor: 'pointer',
         }}>
         프로필 조회
       </button>
@@ -125,12 +126,12 @@ function Login() {
         style={{
           minWidth: 250,
           marginTop: 10,
-          padding: "10px 20px",
+          padding: '10px 20px',
           borderRadius: 5,
-          backgroundColor: "#F1DC11",
+          backgroundColor: '#F1DC11',
           fontSize: 20,
-          border: "none",
-          cursor: 'pointer'
+          border: 'none',
+          cursor: 'pointer',
         }}>
         링크 해제
       </button>
@@ -139,12 +140,12 @@ function Login() {
         style={{
           minWidth: 250,
           marginTop: 10,
-          padding: "10px 20px",
+          padding: '10px 20px',
           borderRadius: 5,
-          backgroundColor: "#F1DC11",
+          backgroundColor: '#F1DC11',
           fontSize: 20,
-          border: "none",
-          cursor: 'pointer'
+          border: 'none',
+          cursor: 'pointer',
         }}>
         로그아웃
       </button>
---
+++ src/index.web.ts
@@ -0,0 +1,72 @@
+import {KaKaoLoginWebType, KakaoLoginModuleInterface} from './types';
+
+const WebKakaoLogins: KakaoLoginModuleInterface = {
+  login(props?: KaKaoLoginWebType) {
+    if (!props) {
+      throw new Error('Web parameters are not provided');
+    }
+
+    const {restApiKeyWeb, redirectUrlWeb, codeWeb} = props;
+
+    if (!restApiKeyWeb || !redirectUrlWeb || !codeWeb) {
+      throw new Error('Web parameters are not provided');
+    }
+
+    const data: any = {
+      grant_type: 'authorization_code',
+      client_id: restApiKeyWeb,
+      redirect_uri: redirectUrlWeb,
+      code: codeWeb,
+    };
+
+    const queryString = Object.keys(data)
+      .map(
+        (k: any) => encodeURIComponent(k) + '=' + encodeURIComponent(data[k]),
+      )
+      .join('&');
+
+    return fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'post',
+      body: queryString,
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    }).then((res) => res.json());
+  },
+  logout(tokenWeb?: string) {
+    return fetch('https://kapi.kakao.com/v1/user/logout', {
+      method: 'post',
+      headers: {Authorization: `Bearer ${tokenWeb}`},
+    }).then((res) => res.json());
+  },
+  unlink(tokenWeb?: string) {
+    return fetch('https://kapi.kakao.com/v1/user/unlink', {
+      method: 'post',
+      headers: {Authorization: `Bearer ${tokenWeb}`},
+    }).then((res) => res.json());
+  },
+  getProfile(tokenWeb?: string) {
+    return fetch('https://kapi.kakao.com/v2/user/me', {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${tokenWeb}`,
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    }).then((res) => res.json());
+  },
+  async getAccessToken() {
+    throw new Error('Web does not support `getAccessToken`');
+  },
+  async loginWithKakaoAccount() {
+    throw new Error('Web does not support `loginWithKakaoAccount`');
+  },
+};
+
+export const login = WebKakaoLogins.login;
+export const loginWithKakaoAccount = WebKakaoLogins.loginWithKakaoAccount;
+export const logout = WebKakaoLogins.logout;
+export const unlink = WebKakaoLogins.unlink;
+export const getProfile = WebKakaoLogins.getProfile;
+export const getAccessToken = WebKakaoLogins.getAccessToken;
+
+export * from './types';
